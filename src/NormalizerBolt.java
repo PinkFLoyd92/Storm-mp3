d52c189e6a2d401e8c7ca828a83b5d0326f9003e
PinkFloyd92 <ascacere92@gmail.com>
2016-01-09 10:55:23 -0500

    added normalizerbol

--- src/NormalizerBolt.java
+++ src/NormalizerBolt.java
@@ -12,31 +12,48 @@ import java.util.List;
  * A bolt that normalizes the words, by removing common words and making them lower case.
  */
 public class NormalizerBolt extends BaseBasicBolt {
-  private List<String> commonWords = Arrays.asList("the", "be", "a", "an", "and",
-      "of", "to", "in", "am", "is", "are", "at", "not", "that", "have", "i", "it",
-      "for", "on", "with", "he", "she", "as", "you", "do", "this", "but", "his",
-      "by", "from", "they", "we", "her", "or", "will", "my", "one", "all", "s", "if",
-      "any", "our", "may", "your", "these", "d" , " ", "me" , "so" , "what" , "him" );
+    private List<String> commonWords = Arrays.asList("the", "be", "a", "an", "and",
+                                                     "of", "to", "in", "am", "is", "are", "at", "not", "that", "have", "i", "it",
+                                                     "for", "on", "with", "he", "she", "as", "you", "do", "this", "but", "his",
+                                                     "by", "from", "they", "we", "her", "or", "will", "my", "one", "all", "s", "if",
+                                                     "any", "our", "may", "your", "these", "d" , " ", "me" , "so" , "what" , "him" );
 
-  @Override
-  public void execute(Tuple tuple, BasicOutputCollector collector) {
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
 
-    /*
-    ----------------------TODO-----------------------
-    Task:
-     1. make the words all lower case
-     2. remove the common words
+        /*
+          ----------------------TODO-----------------------
+          Task:
+          1. make the words all lower case
+          2. remove the common words
 
-    ------------------------------------------------- */
+          ------------------------------------------------- */
+        String sentence = tuple.getString(0);
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            word = word.trim();
+            if(!word.isEmpty() && !this.checkWord(commonWords,word)){
+                word = word.toLowerCase();
+                collector.emit(new Values(word));
+            }
+        }
 
-      
 
-  }
 
-  @Override
-  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    }
+    public boolean checkWord(List<String> words,String word_to_check)
+    {
+	for (String word: words ) {
+	    if (word_to_check.equals(word))
+		return true;
+	}
+	return false;
 
-    declarer.declare(new Fields("word"));
+    }
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
 
-  }
+        declarer.declare(new Fields("word"));
+
+    }
 }
